e32100637cb62d84fa8078c507a7fbcda4cdaced
zhangenming <282126346@qq.com>
2021-12-22 12:02:00 +0800

    `

--- packages/runtime-core/src/h.ts
+++ packages/runtime-core/src/h.ts
@@ -194,17 +194,17 @@ export function h(type: any, propsOrChildren?: any, ...children: any): VNode {
     // }
 
     // // 化简1
-    // if (!(l > 3 || (l === 3 && isVNode(children[0])))) {
-    //   children = children[0]
-    // }
+    if (!(l > 3 || (l === 3 && isVNode(children[0])))) {
+      children = children[0]
+    }
     // // 化简2
     // if (!(l > 3) && !(l === 3 && isVNode(children[0]))) {
     //   children = children[0]
     // }
     // // 化简3
-    if (l <= 3 && (l !== 3 || !isVNode(children[0]))) {
-      children = children[0]
-    }
+    // if (l <= 3 && (l !== 3 || !isVNode(children[0]))) {
+    //   children = children[0]
+    // }
 
     // false false
     // !(true || true)
