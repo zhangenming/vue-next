85e9725e1ea5b39eeeef196b8824f75e7107f771
zhangenming <282126346@qq.com>
2021-12-22 17:07:23 +0800

    `

--- packages/runtime-core/src/h.ts
+++ packages/runtime-core/src/h.ts
@@ -171,9 +171,8 @@ export function h<P>(
 ): VNode
 
 // Actual implementation
-export function h(...childrens: any): VNode {
-  const l = childrens.length
-  const [type, propsOrChildren, children] = childrens
+export function h(type: any, propsOrChildren?: any, ...children: any): VNode {
+  const l = arguments.length
   if (l === 2) {
     if (isObject(propsOrChildren) && !isArray(propsOrChildren)) {
       // single vnode without props
