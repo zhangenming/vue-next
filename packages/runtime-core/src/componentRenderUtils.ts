46ee9d1faff16672ab4d1b9250362d407a1285e8
zhangenming <282126346@qq.com>
2022-01-07 23:51:59 +0800

    `

--- packages/runtime-core/src/componentRenderUtils.ts
+++ packages/runtime-core/src/componentRenderUtils.ts
@@ -72,15 +72,7 @@ export function renderComponentRoot(
       // runtime-compiled render functions using `with` block.
       const proxyToUse = withProxy || proxy
       result = normalizeVNode(
-        render!.call(
-          proxyToUse,
-          proxyToUse!,
-          renderCache,
-          props,
-          setupState,
-          data,
-          ctx
-        )
+        render!(proxyToUse!, renderCache, props, setupState, data, ctx)
       )
       fallthroughAttrs = attrs
     } else {
