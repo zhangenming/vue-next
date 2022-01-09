e5186eb421ca50ca81ad030bb63b886571906ac9
zhangenming <282126346@qq.com>
2022-01-09 22:41:34 +0800

    `

--- packages/runtime-core/src/profiling.ts
+++ packages/runtime-core/src/profiling.ts
@@ -1,29 +1,28 @@
 import { ComponentInternalInstance, formatComponentName } from './component'
 import { devtoolsPerfEnd, devtoolsPerfStart } from './devtools'
 
-let supported: boolean
 let perf: any
-
+let timer: any
 if (typeof window !== 'undefined' && window.performance) {
-  supported = true
   perf = window.performance
+  timer = perf.now
 } else {
-  supported = false
+  timer = Date.now
 }
 
 export function startMeasure(
   instance: ComponentInternalInstance,
   type: string
 ) {
-  if (instance.appContext.config.performance && supported) {
+  if (perf && instance.appContext.config.performance) {
     perf.mark(`vue-${type}-${instance.uid}`)
   }
 
-  devtoolsPerfStart(instance, type, supported ? perf.now() : Date.now())
+  devtoolsPerfStart(instance, type, timer())
 }
 
 export function endMeasure(instance: ComponentInternalInstance, type: string) {
-  if (instance.appContext.config.performance && supported) {
+  if (perf && instance.appContext.config.performance) {
     const startTag = `vue-${type}-${instance.uid}`
     const endTag = startTag + `:end`
     perf.mark(endTag)
@@ -36,5 +35,5 @@ export function endMeasure(instance: ComponentInternalInstance, type: string) {
     perf.clearMarks(endTag)
   }
 
-  devtoolsPerfEnd(instance, type, supported ? perf.now() : Date.now())
+  devtoolsPerfEnd(instance, type, timer())
 }
